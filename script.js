18c74ec8d46fa23d817b8546c2103a35cc510c63
twan507 <120250216+twan507@users.noreply.github.com>
2023-11-02 23:02:52 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 // Hàm cập nhật kích thước iframe
+// function updateIframeSize() {
+//     var origin_height = 3660;
+//     const origin_width = 1200;
+//     const iframe = document.querySelector('.iframe-style');
+//     const currentWidth = window.innerWidth;
+  
+//     if (currentWidth < origin_width) {
+//       const newHeight = origin_height - 3.08 * (origin_width - currentWidth);
+//       iframe.style.height = `${newHeight}px`;
+//     } else {
+//       iframe.style.height = `${origin_height}px`;
+//     }
+//   }
+  
+//   // Thêm event listener để cập nhật kích thước khi cửa sổ được resize
+//   window.addEventListener('resize', updateIframeSize);
+  
+//   // Cập nhật kích thước ban đầu khi tải trang
+//   document.addEventListener('DOMContentLoaded', updateIframeSize);
+
+
 function updateIframeSize() {
-    var origin_height = 3660;
-    const origin_width = 1200;
-    const iframe = document.querySelector('.iframe-style');
-    const currentWidth = window.innerWidth;
-  
-    if (currentWidth < origin_width) {
-      const newHeight = origin_height - 3.08 * (origin_width - currentWidth);
-      iframe.style.height = `${newHeight}px`;
-    } else {
-      iframe.style.height = `${origin_height}px`;
-    }
-  }
-  
-  // Thêm event listener để cập nhật kích thước khi cửa sổ được resize
-  window.addEventListener('resize', updateIframeSize);
-  
-  // Cập nhật kích thước ban đầu khi tải trang
-  document.addEventListener('DOMContentLoaded', updateIframeSize);
-  
+  const originHeight = 3660;
+  const originWidth = 1200;
+  const iframe = document.querySelector('.iframe-style');
+  const currentWidth = window.innerWidth;
+  const scaleFactor = currentWidth < originWidth ? (currentWidth / originWidth) : 1;
+
+  iframe.style.height = `${originHeight * scaleFactor}px`;
+}
+
+// Thêm event listener để cập nhật kích thước khi cửa sổ được resize
+window.addEventListener('resize', updateIframeSize);
+
+// Cập nhật kích thước ban đầu khi tải trang
+document.addEventListener('DOMContentLoaded', updateIframeSize);
+
