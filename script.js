4cb5b81f65f42dfe3346ca33e08aab44c7349248
twan507 <120250216+twan507@users.noreply.github.com>
2023-11-02 14:22:57 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// Hàm cập nhật kích thước iframe
+function updateIframeSize() {
+    var origin_height = 3660;
+    const origin_width = 1200;
+    const iframe = document.querySelector('.iframe-style');
+    const currentWidth = window.innerWidth;
+  
+    if (currentWidth < origin_width) {
+      const newHeight = origin_height - 3.08 * (origin_width - currentWidth);
+      iframe.style.height = `${newHeight}px`;
+    } else {
+      iframe.style.height = `${origin_height}px`;
+    }
+  }
+  
+  // Thêm event listener để cập nhật kích thước khi cửa sổ được resize
+  window.addEventListener('resize', updateIframeSize);
+  
+  // Cập nhật kích thước ban đầu khi tải trang
+  document.addEventListener('DOMContentLoaded', updateIframeSize);
+  
