75f8907ea41b4fc01db1bbacb90cddf15f22dd51
twan507 <120250216+twan507@users.noreply.github.com>
2023-11-03 10:17:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 function updateIframeSize() {
-  const originWidth = 1200; // Chiều rộng ban đầu mà bạn muốn giữ tỷ lệ
-  const originHeight = 3600; // Chiều cao ban đầu mà bạn muốn giữ tỷ lệ
-  const iframe = document.querySelector('.iframe-style'); // Lấy iframe từ trang
-  const currentWidth = window.innerWidth; // Lấy chiều rộng hiện tại của cửa sổ
+  const iframe = document.querySelector('.iframe-style');
+  // Lấy giá trị từ thuộc tính width và height của thẻ iframe
+  const originWidth = parseInt(iframe.width, 10);
+  const originHeight = parseInt(iframe.height, 10);
 
-  let newHeight;
+  const currentWidth = window.innerWidth;
+  let newHeight = originHeight;
 
   if (currentWidth < originWidth) {
     // Tính toán sự thay đổi chiều cao dựa trên mỗi pixel giảm của chiều rộng
-    const widthDecrease = originWidth - currentWidth; // Lượng pixel giảm của chiều rộng
-    newHeight = originHeight - (widthDecrease * 3); // Giảm 3 pixels chiều cao cho mỗi pixel giảm chiều rộng
-  } else {
-    // Nếu chiều rộng hiện tại lớn hơn hoặc bằng chiều rộng ban đầu, giữ nguyên chiều cao ban đầu
-    newHeight = originHeight;
+    const widthDecrease = originWidth - currentWidth;
+    newHeight -= widthDecrease * 3; // Giảm 3 pixels chiều cao cho mỗi pixel giảm chiều rộng
   }
 
   // Đặt chiều cao mới cho iframe
