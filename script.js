0ccd9b198f2ff570078724c13ecdd84fdf04aeb7
twan507 <120250216+twan507@users.noreply.github.com>
2023-11-03 10:51:06 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Hàm cập nhật kích thước iframe
 function updateIframeSize() {
   const iframe = document.querySelector('.iframe-style');
   // Lấy giá trị từ thuộc tính width và height của thẻ iframe
@@ -10,7 +11,8 @@ function updateIframeSize() {
   if (currentWidth < originWidth) {
     // Tính toán sự thay đổi chiều cao dựa trên mỗi pixel giảm của chiều rộng
     const widthDecrease = originWidth - currentWidth;
-    newHeight -= widthDecrease * 3; // Giảm 3 pixels chiều cao cho mỗi pixel giảm chiều rộng
+    // newHeight -= widthDecrease * 1.8;
+    newHeight -= widthDecrease * (originHeight/originWidth - 0.06);
   }
 
   // Đặt chiều cao mới cho iframe
@@ -19,3 +21,36 @@ function updateIframeSize() {
 
 window.addEventListener('resize', updateIframeSize); // Thêm event listener để cập nhật kích thước khi cửa sổ được resize
 document.addEventListener('DOMContentLoaded', updateIframeSize); // Cập nhật kích thước ban đầu khi tải trang
+
+
+// Hàm chuyển đổi page
+document.addEventListener('DOMContentLoaded', (event) => {
+  const navLinks = document.querySelectorAll('nav a');
+
+  navLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault(); // Ngăn chặn sự kiện mặc định của thẻ <a>
+
+      // Lấy giá trị sau dấu '#' và thêm '.html' vào sau để tạo thành tên file
+      const page = link.getAttribute('href').substring(1) + '.html';
+
+      // Chuyển hướng đến trang tương ứng
+      window.location.href = page;
+    });
+  });
+});
+
+// Hàm sử dụng màn hình đen che phần loading
+document.addEventListener('DOMContentLoaded', (event) => {
+  // Hiển thị overlay ngay khi trang được tải
+  const overlay = document.getElementById('overlay');
+  overlay.style.display = 'block';
+
+  // Ẩn overlay sau 3 giây
+  setTimeout(() => {
+    overlay.style.display = 'none';
+  }, 3000); // 3000 milliseconds = 3 seconds
+});
+
+
+
