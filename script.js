6904baa1b924dc970950c08dc079d00544f4ba25
twan507 <120250216+twan507@users.noreply.github.com>
2023-11-03 11:11:22 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -53,4 +53,28 @@ document.addEventListener('DOMContentLoaded', (event) => {
 });
 
 
+// Hàm thêm hình chữ nhật màu đen vào cuối container
+function addOverlayToContainer() {
+  // Find the container element
+  var container = document.querySelector('.container');
+
+  // Create a new div element for the overlay
+  var overlay = document.createElement('div');
+
+  // Style the overlay
+  overlay.style.position = 'absolute'; // Position it absolutely within the container
+  overlay.style.bottom = '0'; // Align it to the bottom of the container
+  overlay.style.left = '0'; // Align it to the left of the container
+  overlay.style.width = '100%'; // Make it as wide as the container
+  overlay.style.height = '60px'; // Set the height of the overlay
+  overlay.style.backgroundColor = 'rgba(0, 0, 0)'; // Set the background color and opacity
+  overlay.style.zIndex = '1000'; // Ensure it sits on top of other content
+
+  // Append the overlay to the container
+  container.style.position = 'relative'; // Ensure the container can hold absolute elements
+  container.appendChild(overlay);
+}
+
+// Run the function to add the overlay
+addOverlayToContainer();
 
