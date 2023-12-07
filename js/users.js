d68692298252d8340a7f514fedbf28b9576c573a
twan507 <120250216+twan507@users.noreply.github.com>
2023-12-07 09:59:30 +0700

    Add files via upload

--- js/users.js
+++ js/users.js
@@ -2,7 +2,8 @@ var users = {
     "admin": "t2madmin1123",
     "doducthang": "t2mstock",
     "lethinhvuong": "t2mstock",
-    "jokerxyz": "jok001!",
     "thanhnguyen508@gmail.com" : "tha0010",
+    "lequanghuy2793@gmail.com" : "leq0011",
+    "quanghaxd16@gmail.com" : "qua0012",
     "guest": 't2mstock'
 };
