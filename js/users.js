1a67da10754ea43d83cdf7e1d593fc410b1b05d7
twan507 <120250216+twan507@users.noreply.github.com>
2023-12-08 12:15:47 +0700

    Update users.js

--- js/users.js
+++ js/users.js
@@ -8,5 +8,11 @@ var users = {
     "kingpapergame@gmail.com":  "kin0015",
     "vubamung@gmail.com":  "vub0016",
     "phungsondmk@gmail.com":  "phu0018",
+    "maikhachai@gmail.com":  "mai0017",
+    "phungsondmk@gmail.com":  "phu0018",
+    "thangnquoc75@gmail.com":  "tha0019",
+    "kemdua88@gmail.com":  "kem0020",
+    "dovancong.vn@gmail.com":  "dov0021",
+    "nguyenminhchunghp@gmail.com":  "ngu0022",
     "guest": 't2mstock'
 };
